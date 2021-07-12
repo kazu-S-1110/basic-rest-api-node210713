5b75d0ced0987b021b9c79f43d3a49d3797fb720
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-07-13 08:20:23 +0900

    sqlite3との接続、クエリを投げるコードを実装！結構楽しいね♪

--- app/app.js
+++ app/app.js
@@ -1,8 +1,19 @@
 const express = require("express")
 const app = express()
+const sqlite3 = require("sqlite3")
+const dbPath = "app/db/database.sqlite3"
 
-app.get("/api/v1/hello", (req, res) => {
-  res.json({"message": "Hello, World!"})
+// app.get("/api/v1/hello", (req, res) => {
+//   res.json({"message": "Hello, World!"})
+// })
+
+//Get all users
+app.get("/api/v1/users", (req, res) => {
+  const db = new sqlite3.Database(dbPath) //DBへ接続。
+  db.all("SELECT * FROM users", (err, rows) => { //クエリ結果受け取る。エラーならerr,正常なら全てがrowsに入る。
+    res.json(rows)
+  })
+  db.close() 
 })
 
 const port = process.env.PORT || 3000
