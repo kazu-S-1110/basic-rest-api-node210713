2cb2ef816d8b841f75f26f0159af4497d326c5fb
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-07-14 05:55:49 +0900

    検索機能を実装。

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,5 +1,11 @@
 const indexModule = (() => {
+//検索ボタンを押した時に発火するイベントリスナーを定義。
+  document.getElementById("search-btn").addEventListener("click",() => {
+    return searchModule.searchUsers()
+  })
+
+
   //UserモジュールのfetchAllUsersメソッドを呼び出す
+  // console.log("index display")
   return usersModule.fetchAllUsers()
-  console.log("index display")
 })()
